e7441b473b43b68a6d7f7a4c589d6d386dadd786
Reid Evans <revans@tombras.com>
2016-01-29 22:47:35 -0500

    add alternate solution to 1 using lists module

--- ReidEvans-Erlang/one.erl
+++ ReidEvans-Erlang/one.erl
@@ -1,7 +1,8 @@
 -module(one).
--export([solve/0]).
+-export([solveRecursive/0,solve/0]).
 
-solve() -> sumMultiples(lists:seq(1,999)).
+%Solve using recursion
+solveRecursive() -> sumMultiples(lists:seq(1,999)).
 
 sumMultiples(L) -> sumMultiples(L,0).
 
@@ -10,11 +11,18 @@ sumMultiples([H|T], Acc) when (H rem 3 =:= 0) or (H rem 5 =:= 0) ->
   sumMultiples(T, Acc + H);
 sumMultiples([_|T],Acc) -> sumMultiples(T, Acc).
 
+%Solve using lists module
+solve() ->
+	lists:sum(
+		lists:filter(
+			fun(Elem) -> (Elem rem 3 == 0) or (Elem rem 5 == 0) end,
+			lists:seq(1,999)
+		)
+	).
+
+
 
 % Notes:
 % If we list all the natural numbers below 10 that are multiples of 3
 % or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 % Find the sum of all the multiples of 3 or 5 below 1000.
-%
-%  http://www.erlang.org/doc/man/lists.html
-%  http://www.erlang.org/doc/reference_manual/expressions.html
