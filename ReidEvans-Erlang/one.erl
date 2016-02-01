dbf4a5fd4dedffc6009bbdee8cd151a3f4ca5b00
Reid Evans <reidev275@gmail.com>
2016-01-31 20:53:42 -0500

    move description to the top of each file

--- ReidEvans-Erlang/one.erl
+++ ReidEvans-Erlang/one.erl
@@ -1,6 +1,10 @@
 -module(one).
 -export([solveRecursive/0,solve/0]).
 
+% If we list all the natural numbers below 10 that are multiples of 3
+% or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+% Find the sum of all the multiples of 3 or 5 below 1000.
+
 %Solve using recursion
 solveRecursive() -> sumMultiples(lists:seq(1,999)).
 
@@ -19,10 +23,3 @@ solve() ->
 			lists:seq(1,999)
 		)
 	).
-
-
-
-% Notes:
-% If we list all the natural numbers below 10 that are multiples of 3
-% or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-% Find the sum of all the multiples of 3 or 5 below 1000.
