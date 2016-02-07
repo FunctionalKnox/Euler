23dafe8366e01d52dc235aa67e03efb9cf1b0ae6
Tyler Jennings <jenningstcj@gmail.com>
2016-02-07 17:55:37 -0500

    euler 2

--- TylerJennings-FSharp/1.fsx
+++ TylerJennings-FSharp/1.fsx
@@ -1,14 +1,7 @@
-let is3or5multiple i =
+let is3or5multiple (i: int) =
   i % 3 = 0 || i % 5 = 0
 
-[1..999]
-|> Seq.filter is3or5multiple
-|> Seq.reduce (fun p c -> p + c)
-
-
-//var resultsList = List<int>();
-//foreach (var i in [1..999])
-//{
-//    if (i % 3 == 0 || i % 5 == 0) resultsList.Add(i);
-//}
-//resultsList.Sum();
+let x = [1..999]
+        |> Seq.filter is3or5multiple
+        |> Seq.reduce (fun p c -> p + c)
+printfn "Print an integer like this %d" x
