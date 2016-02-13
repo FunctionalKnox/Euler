102232ec0f9eba6bab50919343423568415cfeae
Cameron Presley <cameron@thesoftwarementor.com>
2016-02-13 16:38:31 -0500

    Added solutions for problem 1 and 2.

---
+++ CameronPresley-FSharp/problem_one.fsx
@@ -0,0 +1,8 @@
+﻿let input = [1 .. 999]
+
+let isMultipleOf3 x = x % 3 = 0
+let isMultipleOf5 x = x % 5 = 0
+let isValidNum x = isMultipleOf3 x || isMultipleOf5 x
+
+input |> List.filter(fun x -> isValidNum x) |> List.sum |> printfn "%i"
+
---
+++ CameronPresley-FSharp/problem_two.fsx
@@ -0,0 +1,5 @@
+﻿let fib = Seq.unfold (fun (first,second) ->
+    if (second > 4000000) then None
+    else Some(first + second, (second, first + second))) (1,1)
+
+fib |> Seq.filter(fun x -> x % 2 = 0) |> Seq.sum |> printfn "%A"
