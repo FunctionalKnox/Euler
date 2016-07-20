77caa764cfc1409621d3987f9b5990b5afc85f32
Reid Evans <revans@tombras.com>
2016-07-20 12:18:58 -0400

    port 5 to F#

---
+++ ReidEvans-FSharp/5.fsx
@@ -0,0 +1,27 @@
+let factors n =
+    [1..(n/2)]
+    |> List.filter (fun x -> n % x = 0)
+
+let rec allFactorsOfRange range acc =
+    match range with
+    | [] -> acc |> List.distinct |> List.sort
+    | h::t ->
+        let hFactors = factors h
+        allFactorsOfRange t (List.concat [hFactors; acc])
+
+let rec solveSmarter guess increment numbers =
+    if numbers |> List.forall (fun x -> guess % x = 0)
+    then guess
+    else solveSmarter (guess + increment) increment numbers
+
+let solve numbers =
+    let guess = numbers |> List.max
+    let factors = allFactorsOfRange numbers []
+    let primes = numbers |> List.filter (fun x -> factors |> List.contains x |> not)
+    solveSmarter guess guess primes
+
+solve [1..20]
+
+
+//every day at 4 am
+//0 0 4 * * *
