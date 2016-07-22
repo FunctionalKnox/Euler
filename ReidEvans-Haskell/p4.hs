379ae5715baadc8886eec80a52b5fc0624b03ef8
Reid Evans <revans@tombras.com>
2016-07-22 11:10:58 -0400

    solve 4 with haskell

---
+++ ReidEvans-Haskell/p4.hs
@@ -0,0 +1,11 @@
+{- A palindromic number reads the same both ways. 
+   The largest palindrome made from the product of 
+   two 2-digit numbers is 9009 = 91 × 99.
+   Find the largest palindrome made from the 
+   product of two 3-digit numbers.  -}
+isPalindrome x = x == reverse x
+
+tables x = [(*)] <*> [1..x] <*> [1..x]
+
+solve = maximum . filter (isPalindrome . show) . tables
+
