01a89c8c14289ebc8fa8997220a48a5425830184
Reid Evans <revans@tombras.com>
2016-07-22 13:31:30 -0400

    use map rather than pure . apply

--- ReidEvans-Haskell/p4.hs
+++ ReidEvans-Haskell/p4.hs
@@ -3,9 +3,10 @@
    two 2-digit numbers is 9009 = 91 × 99.
    Find the largest palindrome made from the 
    product of two 3-digit numbers.  -}
+
 isPalindrome x = x == reverse x
 
-tables x = [(*)] <*> [1..x] <*> [1..x]
+tables x = (*) <$> [1..x] <*> [1..x]
 
 solve = maximum . filter (isPalindrome . show) . tables
 
