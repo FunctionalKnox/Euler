aa43dc95c843e19b590a63a120e3d800154fe5a8
Reid Evans <reidev275@gmail.com>
2016-09-15 22:12:56 -0400

    initial solution to 5 in haskell

---
+++ ReidEvans-Haskell/p5.hs
@@ -0,0 +1,17 @@
+{-
+  2520 is the smallest number that can be divided by each of
+  the numbers from 1 to 10 without any remainder.
+
+  What is the smallest positive number that is evenly
+  divisible by all of the numbers from 1 to 20?
+-}
+
+isMultiple n x = n `mod` x == 0
+
+solveHelper list max x = 
+  case all (isMultiple x) list of
+    True -> x
+    False -> solveHelper list max (max + x)
+
+solve x =
+  solveHelper [2..x] x x
